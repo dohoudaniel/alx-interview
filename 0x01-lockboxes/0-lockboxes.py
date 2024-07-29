62b9f9c1c2f6d7c4c273cb4863d816562fc329e6
dohoudaniel <dohoudanielfavour@gmail.com>
2024-07-29 18:25:43 +0100

    Add answer

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Python Lockboxes
+"""
+
+
+def canUnlockAll(boxes):
+    """
+    Unlocking Lockboxes
+    """
+    total_boxes = len(boxes)
+    setofkeys = [0]
+    counter = 0
+    index = 0
+    while index < len(setofkeys):
+        setkey = setofkeys[index]
+        for key in boxes[setkey]:
+            if 0 < key < total_boxes and key not in setofkeys:
+                setofkeys.append(key)
+                counter += 1
+        index += 1
+    return counter == total_boxes - 1
