ec5b030a63bf91522728c593483efd2d757c7bd1
dohoudaniel <dohoudanielfavour@gmail.com>
2024-08-03 17:45:08 +0100

    Add installation script

---
+++ 0x06-starwars_api/install.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# This script installs all the required modules to work with this project
+sudo npm install request --global
+export NODE_PATH=/usr/lib/node_modules
+sudo npm install semistandard --global
